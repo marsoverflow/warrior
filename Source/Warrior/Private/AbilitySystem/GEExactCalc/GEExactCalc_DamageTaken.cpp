edde67f6c02d63956d78333e5ffe53be3178fd65
marsoverflow <pternali.gamedev@gmail.com>
2026-07-02 17:31:06 +0200

    Applying damage in attribute set

--- Source/Warrior/Private/AbilitySystem/GEExactCalc/GEExactCalc_DamageTaken.cpp
+++ Source/Warrior/Private/AbilitySystem/GEExactCalc/GEExactCalc_DamageTaken.cpp
@@ -15,6 +15,8 @@ struct FWarriorDamageCapture
 	
 	FWarriorDamageCapture()
 	{
+		// Target = Bersaglio
+		// Source = Chi ha colpito
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, AttackPower, Source, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DefensePower, Target, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DamageTaken, Target, false);
@@ -63,7 +65,7 @@ void UGEExactCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCusto
 	
 	float SourceAttackPower = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().AttackPowerDef, EvaluateParameters, SourceAttackPower);
-	Debug::Print(TEXT("SourceAttackPower"), SourceAttackPower);
+	//Debug::Print(TEXT("SourceAttackPower"), SourceAttackPower);
 	
 	float BaseDamage = 0.f;
 	int32 UsedLightAttackComboCount = 0;
@@ -74,38 +76,38 @@ void UGEExactCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCusto
 		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Shared_SetByCaller_BaseDamageEffect))
 		{
 			BaseDamage = TagMagnitude.Value;
-			Debug::Print(TEXT("BaseDamage"), BaseDamage);
+			//Debug::Print(TEXT("BaseDamage"), BaseDamage);
 		}
 		
 		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Light))
 		{
 			UsedLightAttackComboCount = TagMagnitude.Value;
-			Debug::Print(TEXT("UsedLightAttackComboCount"), UsedLightAttackComboCount);
+			//Debug::Print(TEXT("UsedLightAttackComboCount"), UsedLightAttackComboCount);
 		}
 		
 		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Heavy))
 		{
 			UsedHeavyAttackComboCount = TagMagnitude.Value;
-			Debug::Print(TEXT("UsedHeavyAttackComboCount"), UsedHeavyAttackComboCount);
+			//Debug::Print(TEXT("UsedHeavyAttackComboCount"), UsedHeavyAttackComboCount);
 		}
 	}
 	
 	float TargetDefensePower = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().DefensePowerDef, EvaluateParameters, TargetDefensePower);
-	Debug::Print(TEXT("TargetDefensePower"), TargetDefensePower);
+	//Debug::Print(TEXT("TargetDefensePower"), TargetDefensePower);
 	
 	if (UsedLightAttackComboCount != 0)
 	{
 		const float DamageIncreasePercentLight = (UsedLightAttackComboCount - 1) * 0.05 + 1.f;
 		BaseDamage *= DamageIncreasePercentLight;
-		Debug::Print(TEXT("ScaledBaseDamageLight"), BaseDamage);
+		//Debug::Print(TEXT("ScaledBaseDamageLight"), BaseDamage);
 	}
 	
 	if (UsedHeavyAttackComboCount != 0)
 	{
 		const float DamageIncreasePercentHeavy = UsedHeavyAttackComboCount * 0.15f + 1.f;
 		BaseDamage *= DamageIncreasePercentHeavy;
-		Debug::Print(TEXT("ScaledBaseDamageHeavy"), BaseDamage);
+		//Debug::Print(TEXT("ScaledBaseDamageHeavy"), BaseDamage);
 	}
 	
 	const float FinalDamageDone = BaseDamage * SourceAttackPower / TargetDefensePower;
@@ -115,7 +117,7 @@ void UGEExactCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCusto
 	{
 		OutExecutionOutput.AddOutputModifier(
 			FGameplayModifierEvaluatedData(
-				GetWarriorDamageCapture().DamageTakenProperty,
+				GetWarriorDamageCapture().DamageTakenProperty, // Questo punta al DamageTaken del bersaglio
 				EGameplayModOp::Override,
 				FinalDamageDone
 			)
--- Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
@@ -3,6 +3,9 @@
 
 #include "AbilitySystem/WarriorAttributeSet.h"
 
+#include "GameplayEffectExtension.h"
+#include "WarriorDebugHelper.h"
+
 UWarriorAttributeSet::UWarriorAttributeSet()
 {
 	InitCurrentHealth(1.f);
@@ -11,4 +14,42 @@ UWarriorAttributeSet::UWarriorAttributeSet()
 	InitMaxRage(1.f);
 	InitAttackPower(1.f);
 	InitDefensePower(1.f);
+}
+
+void UWarriorAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	if (Data.EvaluatedData.Attribute == GetCurrentHealthAttribute())
+	{
+		const float NewCurrentHealth = FMath::Clamp(GetCurrentHealth(), 0.f, GetMaxHealth());
+		SetCurrentHealth(NewCurrentHealth);
+	}
+	
+	if (Data.EvaluatedData.Attribute == GetCurrentRageAttribute())
+	{
+		const float NewCurrentRage = FMath::Clamp(GetCurrentRage(), 0.f, GetMaxRage());
+		SetCurrentRage(NewCurrentRage);
+	}
+	
+	if (Data.EvaluatedData.Attribute == GetDamageTakenAttribute())
+	{
+		const float OldHealth = GetCurrentHealth();
+		const float DamageDone = GetDamageTaken();
+		
+		const float NewCurrentHealth = FMath::Clamp(OldHealth - DamageDone, 0.f, GetMaxHealth());
+		SetCurrentHealth(NewCurrentHealth);
+		
+		const FString DebugString = FString::Printf(
+			TEXT("Old Health: %f, Damage Done: %f, New Current Health: %f"),
+			OldHealth, DamageDone, NewCurrentHealth
+		);
+		
+		Debug::Print(DebugString, FColor::Green);
+		
+		// TODO: Notify the UI
+		
+		if (NewCurrentHealth == 0.f)
+		{
+			// TODO: Handle character death
+		}
+	}
 }
