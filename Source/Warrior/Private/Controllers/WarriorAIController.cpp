5ee0f02b31115cc4ce6e6148d974d72f45182a83
marsoverflow <pternali.gamedev@gmail.com>
2026-07-06 16:02:51 +0200

    Base configuration of AISenseConfig_Sight

--- Source/Warrior/Private/Controllers/WarriorAIController.cpp
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -5,11 +5,35 @@
 
 #include "Navigation/CrowdFollowingComponent.h"
 #include "WarriorDebugHelper.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
-AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
+AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer) : 
+Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
 {
 	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
 	{
 		Debug::Print(TEXT("CrowdFollowingComponent valid"), FColor::Green);
 	}
+	
+	AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("AISenseConfig_Sight");
+	// With these configurations, our enemy are targetting only hostile characters
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	AISenseConfig_Sight->SightRadius = 5000.f;
+	// LoseSightRadius set to 0 means our enemies will never lose sight of our player
+	AISenseConfig_Sight->LoseSightRadius = 0.f;
+	// Vision angle of our enemies. In a stealth game this value should be decreased, for example, 180 degrees
+	AISenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;
+	
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("EnemyPerceptionComponent");
+	EnemyPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &ThisClass::OnEnemyPerceptionUpdated);
+}
+
+// Function called whenever an enemy detects our pawn
+void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
 }
