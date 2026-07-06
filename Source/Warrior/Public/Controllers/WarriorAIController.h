a3191c0c6e40b273affa041d8d88e0690dc8bab0
marsoverflow <pternali.gamedev@gmail.com>
2026-07-06 15:51:40 +0200

    Base configuration of AI Controller.
    - Override AAIController PathFollowingComponent using UCrowdFollowingComponent for a better enemy movements

---
+++ Source/Warrior/Public/Controllers/WarriorAIController.h
@@ -0,0 +1,19 @@
+// Patrizio Ternali All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "WarriorAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	AWarriorAIController(const FObjectInitializer& ObjectInitializer);	
+};
