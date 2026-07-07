9a88b0d90d81231b33355f3f4bcf331e0188ad17
marsoverflow <pternali.gamedev@gmail.com>
2026-07-07 10:36:18 +0200

    Set up AI Avoidance values
    - Avoidance quality
    - Avoidance Group
    - Collision Query Range
    - Velocity BIAS to 0 in Crowd Manager Engine Settings
    - Max Agents Radius to 300,0 in Crowd Manager Engine Settings
    - Enabled Resolve Collision in Crowd Manager Engine Settings
    - Separation Dir Clamp to 0,0 in Crowd Manager Engine Settings

--- Source/Warrior/Private/Controllers/WarriorAIController.cpp
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -12,11 +12,6 @@
 AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer) : 
 Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
 {
-	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
-	{
-		Debug::Print(TEXT("CrowdFollowingComponent valid"), FColor::Green);
-	}
-	
 	AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("AISenseConfig_Sight");
 	// With these configurations, our enemy are targetting only hostile characters
 	AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
@@ -47,6 +42,29 @@ ETeamAttitude::Type AWarriorAIController::GetTeamAttitudeTowards(const AActor& O
 	}
 	
 	return ETeamAttitude::Friendly;
+}
+
+void AWarriorAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+	{
+		CrowdComp->SetCrowdSimulationState(bEnableDetourCrowdAvoidance ? ECrowdSimulationState::Enabled : ECrowdSimulationState::Disabled);
+
+		switch (DetourCrowdAvoidanceQuality)
+		{
+		case 1: CrowdComp->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::Low);	 break;
+		case 2: CrowdComp->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::Medium); break;
+		case 3: CrowdComp->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::Good);   break;
+		case 4: CrowdComp->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::High);   break;
+		default: break;
+		}
+		
+		CrowdComp->SetAvoidanceGroup(1);
+		CrowdComp->SetGroupsToAvoid(1);
+		CrowdComp->SetCrowdCollisionQueryRange(CollisionQueryRange);
+	}
 }
 
 // Function called whenever an enemy detects our pawn
