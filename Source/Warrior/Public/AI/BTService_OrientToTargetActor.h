fdfdcc9b56a73f118ad1b3f9eafbe0eeb0ffa28e
marsoverflow <pternali.gamedev@gmail.com>
2026-07-07 15:03:50 +0200

    Implemented custom BTService to Orient enemy AI character to Target Location

---
+++ Source/Warrior/Public/AI/BTService_OrientToTargetActor.h
@@ -0,0 +1,31 @@
+// Patrizio Ternali All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_OrientToTargetActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UBTService_OrientToTargetActor : public UBTService
+{
+	GENERATED_BODY()
+	
+	UBTService_OrientToTargetActor();
+	
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	
+	virtual FString GetStaticDescription() const override;
+	
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	
+	UPROPERTY(EditAnywhere, Category="Target")
+	FBlackboardKeySelector InTargetActorKey;
+	
+	UPROPERTY(EditAnywhere, Category="Target")
+	float RotationInterpSpeed;
+	
+};
