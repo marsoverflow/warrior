a021da5136173dde1132dca4389613fa7d199c37
marsoverflow <pternali.gamedev@gmail.com>
2026-07-21 11:47:43 +0200

    Implemented function to make enemy effect spec handle

--- Source/Warrior/Private/AbilitySystem/Abilities/WarriorEnemyGameplayAbility.cpp
+++ Source/Warrior/Private/AbilitySystem/Abilities/WarriorEnemyGameplayAbility.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/Abilities/WarriorEnemyGameplayAbility.h"
 
+#include "WarriorGameplayTags.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
 #include "Characters/WarriorEnemyCharacter.h"
 
 AWarriorEnemyCharacter* UWarriorEnemyGameplayAbility::GetEnemyCharacterFromActorInfo()
@@ -18,4 +20,25 @@ AWarriorEnemyCharacter* UWarriorEnemyGameplayAbility::GetEnemyCharacterFromActor
 UEnemyCombatComponent* UWarriorEnemyGameplayAbility::GetEnemyCombatComponentFromActorInfo()
 {
 	return GetEnemyCharacterFromActorInfo()->GetEnemyCombatComponent();
+}
+
+FGameplayEffectSpecHandle UWarriorEnemyGameplayAbility::MakeEnemyDamageEffectSpecHandle(
+	TSubclassOf<UGameplayEffect> EffectClass, const FScalableFloat& InDamageScalableFloat)
+{
+	check(EffectClass);
+	
+	FGameplayEffectContextHandle ContextHandle = GetWarriorAbilitySystemComponentFromActorInfo()->MakeEffectContext();
+	ContextHandle.SetAbility(this);
+	ContextHandle.AddSourceObject(GetAvatarActorFromActorInfo());
+	ContextHandle.AddInstigator(GetAvatarActorFromActorInfo(), GetAvatarActorFromActorInfo());
+	
+	FGameplayEffectSpecHandle EffectSpecHandle = 
+		GetWarriorAbilitySystemComponentFromActorInfo()->MakeOutgoingSpec(EffectClass, GetAbilityLevel(), ContextHandle);
+	
+	EffectSpecHandle.Data->SetSetByCallerMagnitude(
+		WarriorGameplayTags::Shared_SetByCaller_BaseDamageEffect, 
+		InDamageScalableFloat.GetValueAtLevel(GetAbilityLevel())
+	);
+	
+	return EffectSpecHandle;
 }
