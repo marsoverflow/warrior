3b8e2db17a56030e554183b2876978f6d06b6de4
marsoverflow <pternali.gamedev@gmail.com>
2026-07-27 11:57:45 +0200

    Base implementation of custom BTTaskNode to rotate pawn to target actor

---
+++ Source/Warrior/Public/AI/BTTask_RotateToFaceTarget.h
@@ -0,0 +1,50 @@
+// Patrizio Ternali All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_RotateToFaceTarget.generated.h"
+
+struct FRotateToFaceTargetTaskMemory
+{
+	TWeakObjectPtr<APawn> OwningPawn;
+	TWeakObjectPtr<AActor> TargetActor;
+	
+	bool IsValid() const
+	{
+		return OwningPawn.IsValid() && TargetActor.IsValid();
+	}
+	
+	void Reset()
+	{
+		OwningPawn.Reset();
+		TargetActor.Reset();
+	}
+};
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UBTTask_RotateToFaceTarget : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	UBTTask_RotateToFaceTarget();
+	
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	
+	virtual uint16 GetInstanceMemorySize() const override;
+	
+	virtual FString GetStaticDescription() const override;
+	
+	UPROPERTY(EditAnywhere, Category = "Face Target")
+	float AnglePrecision;
+	
+	UPROPERTY(EditAnywhere, Category = "Face Target")
+	float RotationInterpSpeed;
+	
+	UPROPERTY(EditAnywhere, Category = "Face Target")
+	FBlackboardKeySelector InTargetToFaceKey;
+};
