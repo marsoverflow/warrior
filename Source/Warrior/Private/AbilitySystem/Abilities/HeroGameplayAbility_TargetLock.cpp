5ad0ed1c431b96d8fba9b61374b523db85062bd7
marsoverflow <pternali.gamedev@gmail.com>
2026-08-04 16:35:03 +0200

    Get Available Targets
    - Implemented GetAvailableActorsToLock function to get all available actors in a certain range defined as a TraceBoxSize

--- Source/Warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
+++ Source/Warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
@@ -3,10 +3,15 @@
 
 #include "AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h"
 
+#include "Characters/WarriorHeroCharacter.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "WarriorDebugHelper.h"
+
 void UHeroGameplayAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
-	const FGameplayEventData* TriggerEventData)
+                                                      const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                                      const FGameplayEventData* TriggerEventData)
 {
+	TryLockOnTarget();
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 }
 
@@ -15,4 +20,39 @@ void UHeroGameplayAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandl
 	bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UHeroGameplayAbility_TargetLock::TryLockOnTarget()
+{
+	GetAvailableActorsToLock();
+}
+
+void UHeroGameplayAbility_TargetLock::GetAvailableActorsToLock()
+{
+	TArray<FHitResult> BoxTraceHits;
+	UKismetSystemLibrary::BoxTraceMultiForObjects(
+		GetHeroCharacterFromActorInfo(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation() + GetHeroCharacterFromActorInfo()->GetActorForwardVector() * BoxTraceDistance,
+		TraceBoxSize / 2,
+		GetHeroCharacterFromActorInfo()->GetActorForwardVector().ToOrientationRotator(),
+		BoxTraceChannel,
+		false,
+		TArray<AActor*>(),
+		bShowPersistentDebugShape ? EDrawDebugTrace::Persistent : EDrawDebugTrace::None,
+		BoxTraceHits,
+		true
+	);
+	
+	for (const FHitResult& TraceHit : BoxTraceHits)
+	{
+		if (AActor* HitActor = TraceHit.GetActor())
+		{
+			if (HitActor != GetHeroCharacterFromActorInfo())
+			{
+				AvailableActorsToLock.AddUnique(HitActor);
+				Debug::Print(HitActor->GetActorNameOrLabel());
+			}
+		}
+	}
 }
